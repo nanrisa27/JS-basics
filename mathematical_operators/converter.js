401fa52c4d2127783a6a999a3818f232d70ef4ca
Minyoi87 <59666333+Minyoi87@users.noreply.github.com>
2020-09-14 09:50:30 +0300

    solution for length converter

---
+++ mathematical_operators/converter.js
@@ -0,0 +1,27 @@
+var input = getElementbyId(input);
+var result = getElementbyId(result);
+var inputType = getElementbyId(inputType);
+var inputType2 = getElementbyId(inputType2);
+
+var inputTypevalue, inputType2value;
+
+//adding an event listener
+input.addEventListener("keyup", myresults);
+inputType.addEventListener("change", myresults);
+inputType2.addEventListener("change", myresults);
+
+//taking initial values
+inputTypevalue = inputType.value
+inputTypevalue = inputType2.value
+
+
+function myresults() {
+    inputTypevalue = inputType.value
+    inputTypevalue = inputType2.value
+
+
+    if () {
+
+    }
+
+}
